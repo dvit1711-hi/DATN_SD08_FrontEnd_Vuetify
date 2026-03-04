a9bd833529dde2d896c901595aaabedcdc8083e5
dvit1711-hi <vietnd1711@gmai.com>
2026-03-04 21:57:55 +0700

    bat dau lai nhe cac con

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+/// <reference types="unplugin-vue-router/client" />
+/// <reference types="vite-plugin-vue-layouts-next/client" />
+
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+/**
+ * router/index.ts
+ *
+ * Manual routes for ./src/pages/*.vue
+ */
+
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from '@/pages/index.vue'
+import Menu from '@/pages/menu.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: Index,
+    },
+    {
+      path: '/menu',
+      component: Menu,
+    },
+  ],
+})
+
+export default router
