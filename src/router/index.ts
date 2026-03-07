3e2c77e467eea50b4d39939e6740d8d2b4f5ae3c
dvit1711-hi <vietnd1711@gmai.com>
2026-03-07 09:39:34 +0700

    Product detail chx xong

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,25 +11,19 @@ import Menu from '@/pages/menu.vue'
 
 import ProductList from '@/pages/ProductList.vue'
 import Footer from '@/layouts/components/Footer.vue'
+import ProductDetail from '@/pages/ProductDetail.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',  component: Index,
-    },
-    {
-      path: '/menu',  component: Menu,
-    },
+    {path: '/',  component: Index},
+    {path: '/menu',  component: Menu},
     
-    {
-      path: '/list',  component: ProductList,
-    },
-    {
-      path: '/footer',
-      component: Footer,
-    },
+    {path: '/products',  component: ProductList},
+    {path: '/products/:id',  component: ProductDetail},
+    
+    {path: '/footer',     component: Footer},
   ],
 })
 
